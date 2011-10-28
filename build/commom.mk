53e7454e153f715e26a134cde14eb68d0857da3a
robin <robin@localhost.localdomain>
2011-10-28 09:50:43 +0800

    add common.mk

---
+++ build/commom.mk
@@ -0,0 +1,24 @@
+MV           := mv -f
+RM           := rm -f
+SED          := sed
+objects      := $(subst .c,.o,$(sources))
+dependencies := $(subst .c,.d,$(sources))
+include_dirs := .. ../../include
+CPPFLAGS     += $(addprefix -I ,$(include_dirs))  -MD
+CFLAGS       += $(addprefix -I ,$(include_dirs))  -MD
+
+vpath %.h $(include_dirs)
+
+.PHONY:   lib 
+
+lib: $(library)
+$(library):  $(objects) 
+	@$(AR) $(ARFLAGS) $@ $^
+
+.PHONY: clean
+clean:
+	$(RM) $(objects) $(program) $(library) $(dependencies) $(extra_clean)
+
+ifneq "$(MAKECMDGOALS)" "clean"
+	-include $(dependencies)
+endif
